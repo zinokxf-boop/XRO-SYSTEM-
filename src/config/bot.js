dc1d8abd183d6203f5f66c3aef93cb2fbbb8ed58
zinokxf-boop <zinokxf@gmail.com>
2026-06-16 10:39:21 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "M8 IS BACK",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
